1e7e79ffabe3b9a44a1616821b1a78de6aa7b545
Seah <blueseaangel@likelion.org>
2018-01-30 05:05:52 +0000

    세아최종

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'devise-bootstrap-views'
 gem 'devise'
 gem 'rails_db'
+gem 'cancancan'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180119134027) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.boolean  "admin"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
